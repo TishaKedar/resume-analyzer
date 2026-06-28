f10469e430f848d4d25e08a442bcf12fa4e4ffd6
Tisha Kedar <23amtics147@gmail.com>
2026-06-28 19:19:51 +0530

    fix url issue

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const WEBHOOK_URL = "http://localhost:5678/webhook/resume-analyzer"
+const WEBHOOK_URL = "https://n8n-production-03b7.up.railway.app/webhook/resume-analyzer"
 export async function analyzeResume(resume, jobDescriptions) {
   const response = await axios.post(WEBHOOK_URL, {
     resume,
