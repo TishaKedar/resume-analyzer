4b93c8a07fd8bd914d2e43178a2fd801379cc1b0
Tisha Kedar <23amtics147@gmail.com>
2026-06-28 19:13:38 +0530

    connected n8n backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const WEBHOOK_URL = "http://localhost:5678/webhook-test/resume-analyzer";
-
+const WEBHOOK_URL = "http://localhost:5678/webhook/resume-analyzer"
 export async function analyzeResume(resume, jobDescriptions) {
   const response = await axios.post(WEBHOOK_URL, {
     resume,
